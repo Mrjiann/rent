7fb1484fc47a1e128e337d1a3f136a88834af762
zezeze <1529290642@qq.com>
2022-02-14 23:33:16 +0800

    导航封装完成一半

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="App">
-    <tab-bar/>
+    <tab-bar>
+      <tab-bar-item>
+        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
 import TabBar from './components/tabber/TabBar.vue'
+import TabBarItem from './components/tabber/TabBarItem.vue';
 
 export default {
   name: "App",
   components: {
-    TabBar
+    TabBar,
+    TabBarItem
   },
 };
 </script>
--- src/components/tabber/TabBar.vue
+++ src/components/tabber/TabBar.vue
@@ -1,16 +1,12 @@
 <template>
   <div id="tab-bar">
-    <span class="tab-bar-item">首页</span>
-    <span class="tab-bar-item">消息</span>
-    <span class="tab-bar-item">发布</span>
-    <span class="tab-bar-item">推荐</span>
-    <span class="tab-bar-item">我的</span>
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-    name:"TabBar"
+  name: "TabBar",
 };
 </script>
 
@@ -25,11 +21,5 @@ export default {
 
   box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.4);
 }
-.tab-bar-item {
-  flex: 1;
-  text-align: center;
-  /* 一般tab-bar高度是49px */
-  height: 49px;
-  line-height: 49px;
-}
+
 </style>
